a2f1713035bc636dffd92e4aa1341a3111a86fa5
Roxxouille <raphael.huiban.le.corff@gmail.com>
2023-12-11 15:50:43 +0100

    :sparkles: added day 9 answer

--- puzzles/2023/day-9/index.ts
+++ puzzles/2023/day-9/index.ts
@@ -11,11 +11,55 @@ async function getInput(filename: Filename): Promise<Input> {
 }
 
 function challenge1(input: Input) {
-  return input.map((line) => line.split(" ").map(Number));
+  return input
+    .map((line) => line.split(" ").map(Number))
+    .map((sequence) => {
+      const rows = [sequence];
+
+      for (let r = 0; ; r++) {
+        const row: number[] = [];
+        for (let i = 0; i < rows[r].length - 1; i++) {
+          row.push(rows[r][i + 1] - rows[r][i]);
+        }
+        rows.push(row);
+        if (row.every((e) => e === 0)) {
+          break;
+        }
+      }
+
+      for (let r = rows.length - 1; r >= 0; r--) {
+        rows[r].push((rows.at(r + 1)?.at(-1) ?? 0) + rows[r].at(-1)!);
+      }
+
+      return rows[0].at(-1)!;
+    })
+    .reduce((a, b) => a + b, 0);
 }
 
 function challenge2(input: Input) {
-  return input;
+  return input
+    .map((line) => line.split(" ").map(Number))
+    .map((sequence) => {
+      const rows = [sequence];
+
+      for (let r = 0; ; r++) {
+        const row: number[] = [];
+        for (let i = 0; i < rows[r].length - 1; i++) {
+          row.push(rows[r][i + 1] - rows[r][i]);
+        }
+        rows.push(row);
+        if (row.every((e) => e === 0)) {
+          break;
+        }
+      }
+
+      for (let r = rows.length - 1; r >= 0; r--) {
+        rows[r].unshift(rows[r][0] - (rows.at(r + 1)?.[0] ?? 0));
+      }
+
+      return rows[0][0];
+    })
+    .reduce((a, b) => a + b, 0);
 }
 
 const [input, sample] = await Promise.all([getInput("input.txt"), getInput("sample.txt")]);
@@ -23,4 +67,6 @@ const [input, sample] = await Promise.all([getInput("input.txt"), getInput("samp
 console.log({
   "Part 1 (sample)": challenge1(sample),
   "Part 1 (final)": challenge1(input),
+  "Part 2 (sample)": challenge2(sample),
+  "Part 2 (final)": challenge2(input),
 });
