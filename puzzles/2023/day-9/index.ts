1a8fe07e0792e3924e04d43cc74d80e6b77e0d19
Roxxouille <raphael.huiban.le.corff@gmail.com>
2023-12-11 15:38:15 +0100

    :sparkles: added day 8 answer

---
+++ puzzles/2023/day-9/index.ts
@@ -0,0 +1,26 @@
+import { readData } from "../../../utils/readData";
+
+type Input = string[];
+type Filename = "input.txt" | "sample.txt";
+
+const year = 2023;
+const day = 9;
+
+async function getInput(filename: Filename): Promise<Input> {
+  return await readData({ year, day, filename });
+}
+
+function challenge1(input: Input) {
+  return input.map((line) => line.split(" ").map(Number));
+}
+
+function challenge2(input: Input) {
+  return input;
+}
+
+const [input, sample] = await Promise.all([getInput("input.txt"), getInput("sample.txt")]);
+
+console.log({
+  "Part 1 (sample)": challenge1(sample),
+  "Part 1 (final)": challenge1(input),
+});
